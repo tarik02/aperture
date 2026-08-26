ca21f1af781e6416c05397b79c4a14bc699acfa8
Taras <Taras.Fomin@gmail.com>
2026-08-26 14:31:32 +0300

    fix: release implicit control when hidden

--- web/src/components/workbench/browser-viewport.tsx
+++ web/src/components/workbench/browser-viewport.tsx
@@ -275,6 +275,7 @@ export function BrowserViewport({
     const handleVisibilityChange = () => {
       if (document.visibilityState === "hidden") {
         releasePressedKeys();
+        releaseImplicitControl();
       }
     };
 
