7d62322d53dbebff5360d124e8904cb2069aca35
tarik02 <Taras.Fomin@gmail.com>
2026-09-20 12:36:34 +0000

    fix: configure browser font fallbacks (#99)
    * fix: configure browser font fallbacks
    
    * refactor: use packaged fontconfig rules

--- flake.nix
+++ flake.nix
@@ -82,9 +82,11 @@
 
         browserFonts = [
           pkgs.dejavu_fonts
+          pkgs.liberation_ttf
           pkgs.noto-fonts
           pkgs.noto-fonts-cjk-sans
           pkgs.noto-fonts-color-emoji
+          pkgs.roboto
         ];
 
         browserFontsConf = pkgs.writeText "aperture-fonts.conf" ''
@@ -99,61 +101,27 @@
               </rescan>
             </config>
 
+            <include>${lib.getOutput "out" pkgs.fontconfig}/etc/fonts/conf.d</include>
+
             <alias binding="strong">
               <family>system-ui</family>
               <prefer><family>Noto Sans</family></prefer>
             </alias>
             <alias binding="strong">
-              <family>ui-sans-serif</family>
+              <family>-apple-system</family>
               <prefer><family>Noto Sans</family></prefer>
             </alias>
             <alias binding="strong">
-              <family>ui-rounded</family>
+              <family>BlinkMacSystemFont</family>
               <prefer><family>Noto Sans</family></prefer>
             </alias>
             <alias binding="strong">
-              <family>sans-serif</family>
-              <prefer>
-                <family>Noto Sans</family>
-                <family>Noto Sans CJK SC</family>
-                <family>Noto Color Emoji</family>
-              </prefer>
+              <family>Segoe UI</family>
+              <prefer><family>Noto Sans</family></prefer>
             </alias>
             <alias binding="strong">
-              <family>ui-serif</family>
-              <prefer><family>Noto Serif</family></prefer>
-            </alias>
-            <alias binding="strong">
-              <family>serif</family>
-              <prefer>
-                <family>Noto Serif</family>
-                <family>Noto Sans CJK SC</family>
-                <family>Noto Color Emoji</family>
-              </prefer>
-            </alias>
-            <alias binding="strong">
-              <family>ui-monospace</family>
-              <prefer><family>Noto Sans Mono</family></prefer>
-            </alias>
-            <alias binding="strong">
-              <family>monospace</family>
-              <prefer>
-                <family>Noto Sans Mono</family>
-                <family>Noto Sans Mono CJK SC</family>
-                <family>Noto Color Emoji</family>
-              </prefer>
-            </alias>
-            <alias binding="strong">
-              <family>emoji</family>
-              <prefer><family>Noto Color Emoji</family></prefer>
-            </alias>
-            <alias binding="strong">
-              <family>Apple Color Emoji</family>
-              <prefer><family>Noto Color Emoji</family></prefer>
-            </alias>
-            <alias binding="strong">
-              <family>Segoe UI Emoji</family>
-              <prefer><family>Noto Color Emoji</family></prefer>
+              <family>Helvetica Neue</family>
+              <prefer><family>Liberation Sans</family></prefer>
             </alias>
             <alias binding="strong">
               <family>Segoe UI Symbol</family>
